0be8264802e78e020ca781cc3100c537c9487cfb
JohnDev <jparocha777@gmail.com>
2025-08-30 21:29:42 -0600

    added distance prop

--- src/Models/Floor/index.tsx
+++ src/Models/Floor/index.tsx
@@ -2,18 +2,18 @@ import { useEffect, useState } from 'react';
 import * as THREE from 'three'
 
 type FloorProps = {
+	distance: number;
   scaleFactor?: number;
   boxDepth?: number;
 };
 
-export const Floor = ({ scaleFactor = 1, boxDepth = 1 }: FloorProps) => {
+export const Floor = ({ scaleFactor = 1, boxDepth = 1, distance }: FloorProps) => {
   const [dimensions, setDimensions] = useState({ width: 0, length: 0 });
 
   useEffect(() => {
     const updateDimensions = () => {
       const aspect = window.innerWidth / window.innerHeight;
       const fov = 75;
-      const distance = 5; // Camera distance from plane
       const length = 2 * Math.tan((fov * Math.PI / 180) / 2) * distance; // Screen height (Y-axis)
       const width = length * aspect; // Screen width (X-axis)
       setDimensions({
@@ -25,7 +25,7 @@ export const Floor = ({ scaleFactor = 1, boxDepth = 1 }: FloorProps) => {
     updateDimensions();
     window.addEventListener('resize', updateDimensions);
     return () => window.removeEventListener('resize', updateDimensions);
-  }, [scaleFactor]);
+  }, [scaleFactor, distance]);
 
   return (
     <mesh position={[0, 0, -boxDepth / 2]}>
--- src/Scene/index.tsx
+++ src/Scene/index.tsx
@@ -5,12 +5,14 @@ import { D20 } from "../Models/Dice/D20";
 import { Floor } from "../Models/Floor";
 
 export const Scene = () => {
+  const distance = 5;
+
   return (
-    <Canvas className={styles.canvas} camera={{ position: [0, 0, 5], fov: 75 }}>
+    <Canvas className={styles.canvas} camera={{ position: [0, 0, distance], fov: 75 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 1, 1]} intensity={1} />
       <D20 />
-      <Floor  />
+      <Floor distance={distance} />
     </Canvas>
   );
 };
