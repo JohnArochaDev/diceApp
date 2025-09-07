f7042dbbf27446f44ba664dd1aa26243175f0048
JohnDev <jparocha777@gmail.com>
2025-09-07 11:56:44 -0600

    added fonts and titles to dropdown

--- src/Components/DiceSelect/index.tsx
+++ src/Components/DiceSelect/index.tsx
@@ -5,6 +5,7 @@ import FormControl from "@mui/material/FormControl";
 import Select, { type SelectChangeEvent } from "@mui/material/Select";
 import { useScene } from "../../Context";
 import { type DiceType } from "../../Context";
+import '@fontsource/roboto/400.css';
 
 export default function DiceSelect() {
   const { selection, setSelection, setSimulate, simulate } = useScene();
@@ -34,6 +35,7 @@ export default function DiceSelect() {
             <InputLabel
               sx={{
                 color: "white",
+                fontFamily: '"Roboto", sans-serif',
                 fontWeight: 400,
                 "&.MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
                   transform: "translate(0, 16px) scale(1)",
@@ -61,6 +63,8 @@ export default function DiceSelect() {
                     backgroundColor: "#2E3B3E",
                     "& .MuiMenuItem-root": {
                       color: "white",
+                      fontWeight: 400,
+                      fontFamily: '"Roboto", sans-serif',
                     },
                   },
                 },
@@ -72,6 +76,8 @@ export default function DiceSelect() {
                 borderRadius: "8px",
                 boxSizing: "border-box",
                 backgroundColor: "#2E3B3E",
+                fontWeight: 400,
+                fontFamily: '"Roboto", sans-serif',
                 "& .MuiOutlinedInput-notchedOutline": {
                   borderColor: "#d4a017",
                 },
@@ -82,20 +88,20 @@ export default function DiceSelect() {
                   borderColor: "#d4a017",
                 },
                 "& .MuiSelect-select": {
-                  textAlign: "center",
                   color: "white",
+                  fontFamily: '"Roboto", sans-serif',
                 },
                 "& .MuiSelect-icon": {
                   color: "#d4a017",
                 },
               }}
             >
-              <MenuItem value={"d20"}>D20</MenuItem>
-              <MenuItem value={"d12"}>D12</MenuItem>
-              <MenuItem value={"d10"}>D10</MenuItem>
-              <MenuItem value={"d8"}>D8</MenuItem>
-              <MenuItem value={"d6"}>D6</MenuItem>
-              <MenuItem value={"d4"}>D4</MenuItem>
+              <MenuItem value={"d20"}>D20 - Icosahedron</MenuItem>
+              <MenuItem value={"d12"}>D12 - Dodecahedron</MenuItem>
+              <MenuItem value={"d10"}>D10 - Pentagonal Trapez</MenuItem>
+              <MenuItem value={"d8"}>D8 - Octahedron</MenuItem>
+              <MenuItem value={"d6"}>D6 - Cube</MenuItem>
+              <MenuItem value={"d4"}>D4 - Tetrahedron</MenuItem>
             </Select>
           </FormControl>
         </Box>
--- src/Components/Title/index.tsx
+++ src/Components/Title/index.tsx
@@ -4,7 +4,7 @@ export const Title = () => {
   return (
     <div className={styles.titleContainer}>
       <img src="/images/D20Icon.png" alt="D20 Icon" className={styles.icon} />
-      <h1 className={styles.title}>DICE ROLLER</h1>
+      <h1 className={styles.title}>DND DICE</h1>
     </div>
   );
 };
